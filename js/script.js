ee020428ed18eec3a4961d11d3558078479843d4
Ednardo Paz <eds_paz@hotmail.com>
2021-06-03 22:33:36 -0300

    controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-import { SlideNav } from './slide.js';
+import  SlideNav  from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
+slide.addControl('.custom-controls');
 
-slide.addControl();
-
-console.log(slide);
